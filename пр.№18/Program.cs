ed10e86f99dad8b90845fe6ddf7d3b5db9ef2e62
user <user@LAPTOP-VCVPT2J9>
2022-12-23 23:58:21 +0300

    Добавьте файлы проекта.

---
+++ пр.№18/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static uint ArithmeticProgression(uint n, uint d)
+    {
+        if (n == 0)
+        {
+            return d;
+        }
+
+        return ArithmeticProgression(n - 1, d) + d;
+    }
+
+
+    static void Main(string[] args)
+    {
+        Console.Write("N = ");
+        var n = Convert.ToUInt32(Console.ReadLine());
+        Console.Write("d = ");
+        var d = Convert.ToUInt32(Console.ReadLine());
+
+        var sum = 0u;
+        for (var i = 0u; i < n; i++)
+        {
+            sum += ArithmeticProgression(i, d);
+        }
+
+        Console.WriteLine($"Сумма {n} первых членов арифметической прогрессии равна {sum}");
+        Console.ReadLine();
+    }
+}
